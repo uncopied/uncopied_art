cc54e0eb4fdff8b37b7ae2a3dd433fbb48c1fd80
NamSor <elian.carsenat@namsor.com>
2021-02-04 20:48:31 +0100

    revert on idea of deploying to Github pages
    revert on idea of deploying to Github pages

--- build_prod.bat
+++
@@ -1,9 +0,0 @@
-REM this deploys the production code from Master branch to https://uncopied.art/ as a Github page
-npm run build
-DEL /S /Q docs
-move build docs
-copy docs\index.html docs\404.html
-copy src\robots.txt docs\robots.txt
-copy src\sitemap.xml docs\sitemap.xml
-copy CNAME docs\CNAME
-REM you need to check that the deployment works and usually re-set Github Pages
